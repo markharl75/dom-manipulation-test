0b568cd6524e6622509594f89bb3b19cdd2c7891
Mark Harland <markharland75@gmail.com>
2021-10-10 18:14:19 -0700

    initial commit

---
+++ fancifymytext.js
@@ -0,0 +1,25 @@
+// Insert javascript here!
+function alertMessage(){
+    // alert("Hello, World!");
+    document.getElementById("input-text").style.fontSize = "24pt";
+}
+
+function boldText(){
+    // alert("Hello from BoldText!");
+    if(document.getElementById("fancyButton").checked == true){
+        document.getElementById("input-text").style.fontWeight = "900";
+        document.getElementById("input-text").style.color = "blue";
+        document.getElementById("input-text").style.textDecoration = "underline";
+    } else if(document.getElementById("boringButton").checked == true){
+        document.getElementById("input-text").style.fontWeight = "100";
+    }
+}
+
+function mooText(){
+    let untrimmedString = document.getElementById("input-text").value;
+    untrimmedString = untrimmedString.toUpperCase();
+    // alert(untrimmedString);
+    var wordsArray = untrimmedString.split(".");
+    let returnStr = wordsArray.join("MOO.");
+    alert(returnStr);
+}
